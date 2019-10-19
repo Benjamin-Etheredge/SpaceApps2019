4b46f951040c655f3f6729630135488c16abebce
Benjamin Etheredge <benjamin.etheredge@gmail.com>
2019-10-19 15:24:02 -0500

    Setting up interface.

---
+++ Filler.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+class Filler:
+    @staticmethod
+    def fill_column(data: pd.DataFrame, column_name: str) -> pd.DataFrame:
+        raise NotImplementedError('Code it up you scrub!')
