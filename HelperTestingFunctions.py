a866950ce4df9711ae7b3aef19a00ed11a27943d
Benjamin Etheredge <benjamin.etheredge@gmail.com>
2019-10-19 16:50:45 -0500

    Adding more stuff

--- HelperTestingFunctions.py
+++ HelperTestingFunctions.py
@@ -1,3 +1,12 @@
 # TODO implement method to get a dataset from sclearn for testing
 def get_testing_dataset():
-    return None
+    import pandas as pd
+    from sklearn.datasets import fetch_california_housing
+    cal_housing = fetch_california_housing()
+    X, y = cal_housing.data, cal_housing.target
+    names = cal_housing.feature_names
+    X = pd.DataFrame(X, columns=names)
+    y = pd.Series(y)
+    data = X
+    data["target"] = y
+    return data, "target"
