ef7af259fcd502930f76007a292f2316c21c5ecb
Matt Camp <usmcamp0811@gmail.com>
2019-10-19 18:53:26 -0500

    added pymonog and hypercot

--- Filler.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-class Filler:
-    @staticmethod
-    def fill_column(data: pd.DataFrame, column_name: str) -> pd.DataFrame:
-        raise NotImplementedError('Code it up you scrub!')
