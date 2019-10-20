d1074f34099f90d996b042c3081ce8048a61b0ce
Matt Camp <usmcamp0811@gmail.com>
2019-10-19 21:07:41 -0500

    damn you pandas

--- fillers/filler_iterative.py
+++ fillers/filler_iterative.py
@@ -11,5 +11,5 @@ class Filler_Iterative(Filler):
     def fill_column(data: pd.DataFrame, column_name: str) -> pd.DataFrame:
         imp = IterativeImputer(max_iter=10, random_state=0)
         imp_data = pd.DataFrame(imp.fit_transform(data), columns = data.columns)
-        data[column_name] = imp_data[column_name]
+        data[column_name] = imp_data[column_name].values
         return data
