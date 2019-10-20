83ad0a6ea493541a1dbc6f3639775201a29c34fa
Benjamin Etheredge <benjamin.etheredge@gmail.com>
2019-10-19 19:10:58 -0500

    Fixing typos

--- fillers/filler_constant.py
+++ fillers/filler_constant.py
@@ -6,9 +6,10 @@ Created on Sat Oct 19 18:02:58 2019
 """
 
 class Filler_Constant(Filler):
+
     @staticmethod
-    allowable_datatypes = ['float64','int64', 'float32','int32', 'string32','string64', 'O']
     def fill_column(data: pd.DataFrame, column_name: str) -> pd.DataFrame:
+        allowable_datatypes = ['float64', 'int64', 'float32', 'int32', 'string32', 'string64', 'O']
         imputer = Imputer(missing_values='NaN', strategy='constant')
         
         if(df[column_name].dtype in allowable_datatypes):
