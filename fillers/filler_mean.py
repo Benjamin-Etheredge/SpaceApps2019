205bacc964a894ad5d73941517d20cb66bb5def9
Benjamin Etheredge <benjamin.etheredge@gmail.com>
2019-10-19 19:52:18 -0500

    Removing depreciated functions.

--- fillers/filler_mean.py
+++ fillers/filler_mean.py
@@ -11,13 +11,16 @@ from fillers import Filler
 import pandas as pd
 
 class Filler_Mean(Filler):
-    @staticmethod
 
+    @staticmethod
     def fill_column(data: pd.DataFrame, column_name: str) -> pd.DataFrame:
+        data[column_name] = data[column_name].fillna(data[column_name].mean())
+        return data
         imputer = Imputer(missing_values='NaN', strategy='mean')
 
+
         allowable_datatypes = ['float64','int64', 'float32','int32']
-        if(df[column_name].dtype in allowable_datatypes):
+        if(data[column_name].dtype in allowable_datatypes):
             imputer.fit(data[column_name].values.reshape(-1, 1))
             data[column_name]= imputer.transform(data[column_name].values.reshape(-1, 1))
         else:
--- fillers/filler_most_frequent.py
+++ fillers/filler_most_frequent.py
@@ -16,10 +16,15 @@ class Filler_MostFrequent(Filler):
 
     def fill_column(data: pd.DataFrame, column_name: str) -> pd.DataFrame:
 
+        data[column_name] = data[column_name].fillna(data[column_name].mode()[0])
+        return data
+        #Jdata[column_name] = data[column_name].fillna(data[column_name].mode())
+        #return data
+
         allowable_datatypes = ['float64','int64', 'float32','int32', 'string32','string64', 'O']
         imputer = Imputer(missing_values='NaN', strategy='most_frequent')
 
-        if(df[column_name].dtype in allowable_datatypes):
+        if(data[column_name].dtype in allowable_datatypes):
             imputer.fit(data[column_name].values.reshape(-1, 1))
             data[column_name]= imputer.transform(data[column_name].values.reshape(-1, 1))
         else:
