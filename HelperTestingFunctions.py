3e7a01dd36f6acc541ff1f3abbed9bf4a20ed3e7
Benjamin Etheredge <benjamin.etheredge@gmail.com>
2019-10-19 22:03:00 -0500

    Adding vanilla data retriever

--- HelperTestingFunctions.py
+++ HelperTestingFunctions.py
@@ -12,6 +12,18 @@ def get_testing_dataset():
     missing_data = make_missing_data(data, n=.25)
     return missing_data, "target"
 
+def get_testing_dataset_vanilla():
+    import pandas as pd
+    from sklearn.datasets import fetch_california_housing
+    cal_housing = fetch_california_housing()
+    X, y = cal_housing.data, cal_housing.target
+    names = cal_housing.feature_names
+    X = pd.DataFrame(X, columns=names)
+    y = pd.Series(y)
+    data = X
+    data["target"] = y
+    return data, "target"
+
 
 def random_missing_ix(data, n=.25, random_state=811):
     if n < 1.0:
