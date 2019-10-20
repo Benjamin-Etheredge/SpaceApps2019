2e7f0fd4ae7d0fd835670c85b80db7c43e2c5afb
Matt Camp <usmcamp0811@gmail.com>
2019-10-19 20:49:58 -0500

    Added filler interative

--- fillers/__init__.py
+++ fillers/__init__.py
@@ -2,4 +2,5 @@ from fillers.filler import Filler
 from fillers.median_filler import MedianFiller
 from fillers.filler_mean import Filler_Mean
 from fillers.filler_most_frequent import Filler_MostFrequent
+from fillers.filler_iterative import Filler_Iterative 
 
